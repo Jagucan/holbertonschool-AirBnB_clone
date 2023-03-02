885df3dfd6b9edbe8445598898a3a6c37603af10
Jagucan <jjgc.2499@gmail.com>
2023-03-02 17:47:30 -0500

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,24 @@
+#!/usr/bin/python3
+""" Base module """
 import uuid
 import datetime
+
 
 class BaseModel:
     """ Base Class """
 
     def __init__(self, *args, **kwargs):
         """ Initialize instance """
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
                         value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ Print str format... """

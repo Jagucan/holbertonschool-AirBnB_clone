bc74df1d32e872acc21e9f1686e75ff4ae40c099
Jagucan <jjgc.2499@gmail.com>
2023-03-02 19:24:41 -0500

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel:
     """ Base Class """
 
     def __init__(self, *args, **kwargs):
+        """ Initialize instance """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.datetime.now()
@@ -36,4 +37,3 @@ class BaseModel:
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
-

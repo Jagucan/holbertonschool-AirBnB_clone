04520af0f124ced8a0e1286db2a9ee6592ca76a4
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-03-03 17:38:05 -0500

    add destroy and all

--- console.py
+++ console.py
@@ -3,9 +3,15 @@
 import cmd
 import shlex
 import models
+from models.base_model import BaseModel
+
+
+def parse(arg):
+    return shlex.split(arg)
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
+    classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """ Quit and exit the program whit quit command """
@@ -31,21 +37,70 @@ class HBNBCommand(cmd.Cmd):
         """ Prints the documentation for help command """
         print("List available commands with \"help\" or detailed help with \"help cmd\"")
 
+    def do_destroy(self, arg):
+        args = parse(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            items = models.storage.all()
+            key = '{}.{}'.format(args[0], args[1])
+            try:
+                item = items[key]
+                del items[key]
+                models.storage.save()
+                del item
+            except KeyError:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        args = parse(arg)
+        items = models.storage.all()
+        item_list = []
+        if len(args) >= 1:
+            if args[0] not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                for key, item in items.items():
+                    if key.startswith(args[0]):
+                        item_list.append(item.__str__())
+                print(item_list)
+        else:
+            for item in items.values():
+                item_list.append(item.__str__())
+            print(item_list)
+
+    def do_show(self, arg):
+        args = parse(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            items = models.storage.all()
+            key = '{}.{}'.format(args[0], args[1])
+            try:
+                item = items[key]
+                print(item)
+            except KeyError:
+                print("** no instance found **")
+
     def do_create(self, arg):
         args = parse(arg)
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in self.__class__:
+        elif args[0] not in self.classes:
             print("** class doesn't exist **")
         else:
             item = eval("{}()".format(args[0]))
             print(item.id)
-            models.storage.save
-    
-def parse(arg):
-    return shlex.split(arg)
+            models.storage.new(item)
+            models.storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-

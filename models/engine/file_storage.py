20a869235af5f6ff49f6d638f942cec7c297851c
Jagucan <jjgc.2499@gmail.com>
2023-03-02 19:16:09 -0500

    File Storage - change on reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
         obj_dict = {}
         for key, obj in FileStorage.__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(FileStorage.__file_path, mode='wd', encoding='utf-8') as f:
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
             json.dump(obj_dict, f)
 
     def reload(self):

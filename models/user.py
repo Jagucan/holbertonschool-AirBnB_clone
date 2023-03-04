e037f71afa50b6bc920cd362134339b8d0e3b98d
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-03-04 11:56:17 -0500

    user update

--- models/user.py
+++ models/user.py
@@ -4,12 +4,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """ Class User """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __init__(self, *args, **kwargs):
+        """ Initialize instance """
         super().__init__(*args, **kwargs)
-        
-        

4a6a584f102fe96889fac54b6e7952499313a3ca
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-03-04 16:13:30 -0500

    test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
 import unittest
-from models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage, BaseModel
 
 
 class Test_FileStorage(unittest.TestCase):
-    """ Test for FileStorage Class """
+    """ Test for FileStorage Class"""
 
-    def test_to_filestorage_creation(self):
-        """ Test for filestorage instance creation """
-        self.assertIsInstance(FileStorage(), FileStorage)
+    def setUp(self):
+        """ Set up test environment """
+        self.__file_path = "test.json"
+        self.storage = FileStorage()
+        self.model = BaseModel()
 
+    def test_to_all(self):
+        """ Test for all method """
+        self.assertEqual(len(self.storage.all()), 0)
+        self.model.save()
+        self.assertEqual(len(self.storage.all()), 1)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_to_new(self):
+        """ Test for new method """
+        self.storage.new(self.model)
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.assertEqual(len(self.storage.all()), 1)
+        self.assertIn(key, self.storage.all().keys())
+
+    def test_to_save(self):
+        """ Test for save method """
+        self.assertEqual(len(self.storage.all()), 0)
+        self.model.save()
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        with open(self.__file_path, "r") as f:
+            data = f.read()
+        self.assertIn(key, data)
+
+    def test_to_reload(self):
+        """ Test for reload method """
+        self.assertEqual(len(self.storage.all()), 0)
+        self.model.save()
+        self.storage.reload()
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.assertEqual(len(self.storage.all()), 1)
+        self.assertIn(key, self.storage.all().keys())
